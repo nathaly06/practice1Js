865c85d1a087160321ebb0c073ec1d0bb8d91daf
erika-nath <enath6.25@gmail.com>
2020-03-25 00:10:37 -0600

    mostrando con innerHTML

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-let name = "Quetzaly Sanchez";
-let height = 180;
+//let name = "Quetzaly Sanchez";
+let name = "Sanchez";
+
+let height = 188;
 
 let concatenation = name +" "+ height;//las comillas hacen un espacio
 
-document.write(concatenation);// document.write es para mostrar en html
+//document.write(concatenation);document.write es para mostrar en html
+
+document.querySelector("#container");   
+container.innerHTML = concatenation;  //con innerHTML puedes seleccionar un elemento del DOM
