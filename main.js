f8fbaf75a230a391cd3887970221e41a552da874
erika-nath <enath6.25@gmail.com>
2020-03-24 23:24:35 -0600

    se agrega un document.write

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ let name = "Quetzaly Sanchez";
 let height = 180;
 
 let concatenation = name +" "+ height;//las comillas hacen un espacio
- console.log(concatenation)
- //first
+
+document.write(concatenation);// document.write es para mostrar en html
